fee4ead75d4d76f061328949a12ac3df030cda01
Iael Leonardo Borda <iaelborda04@gmail.com>
2026-09-05 14:43:32 -0300

    feat: agrego form con lista de clientes y botones crud

---
+++ WindowsForms/ClienteDetalle.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsForms
+{
+    partial class ClienteDetalle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            idLabel = new Label();
+            idTextBox = new TextBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // idLabel
+            // 
+            idLabel.AutoSize = true;
+            idLabel.Location = new Point(20, 30);
+            idLabel.Margin = new Padding(2, 0, 2, 0);
+            idLabel.Name = "idLabel";
+            idLabel.Size = new Size(24, 20);
+            idLabel.TabIndex = 0;
+            idLabel.Text = "ID";
+            // 
+            // idTextBox
+            // 
+            idTextBox.BackColor = Color.LightGray;
+            idTextBox.Location = new Point(100, 30);
+            idTextBox.Name = "idTextBox";
+            idTextBox.ReadOnly = true;
+            idTextBox.Size = new Size(180, 27);
+            idTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(-6, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(66, 97);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // ClienteDetalle
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(432, 503);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(idTextBox);
+            Controls.Add(idLabel);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "ClienteDetalle";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Detalle de Cliente";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label idLabel;
+        private TextBox idTextBox;
+        private Label label1;
+        private TextBox textBox1;
+    }
+}
